a34734883fb53d2419ff829c3afc683b11d2cf68
Samiker69 <Samiker69@github.com>
2025-04-03 20:33:15 +0300

    Fix: configs work correctly. added compatibility with modmenu jvmstatic не бей меня

--- src/client/java/xyz/samiker/pdtd_torch/PutDownTheDamnTorchClient.java
+++ src/client/java/xyz/samiker/pdtd_torch/PutDownTheDamnTorchClient.java
@@ -20,7 +20,7 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
-import xyz.samiker.pdtd_torch.config.PdtdConfig; // Импорт класса конфига
+import xyz.samiker.pdtd_torch.config.PdtdConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import org.lwjgl.glfw.GLFW;
